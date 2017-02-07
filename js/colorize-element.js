968d123931ec9404ab89f7b23183b21430c5a9e6
Илья Молчанов <plue@inbox.ru>
2017-02-07 18:45:47 +0300

    в colorizeElement добавила обработчик по enter

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -10,4 +10,24 @@ window.colorizeElement = function (element, colors, property) {
     currentColor = newColor;
     element.style[property] = currentColor;
   });
+
+
+  element.addEventListener('keydown', function (event) {
+    var newColor = null;
+    while (!newColor || newColor === currentColor) {
+      newColor = window.utils.getRandomElementExcept(colors, currentColor);
+    }
+    currentColor = newColor;
+    element.style[property] = currentColor;
+  });
+
+  // element.addEventListener('click', function () {
+  //   element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
+  // });
+
+  // element.addEventListener('keydown', function (event) {
+  //   if (window.isActivateEvent(event)) {
+  //     element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
+  //   }
+  // });
 };
