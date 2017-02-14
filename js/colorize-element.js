ac0b3be60ef98db39955e5501c71d9adc9f9d776
Илья Молчанов <plue@inbox.ru>
2017-02-14 12:20:35 +0300

    полная изоляция волшебника

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,15 +1,16 @@
 'use strict';
-window.colorizeElement = function (element, colors, property) {
+window.colorizeElement = (function () {
 
-  var currentColor = element.style[property];
-
-  // одна функция на два события клик и нажатие
-  var changeRandomColors = function (event) {
-    if (event.type === 'click' || window.isActivateEvent(event)) {
-      element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
-    }
+  return function (element, colors, property) {
+    var currentColor = element.style[property];
+    // одна функция на два события клик и нажатие
+    var changeRandomColors = function (event) {
+      if (event.type === 'click' || window.keyPress.isActivateEvent(event)) {
+        element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
+      }
+    };
+    element.addEventListener('click', changeRandomColors);
+    element.addEventListener('keydown', changeRandomColors);
   };
 
-  element.addEventListener('click', changeRandomColors);
-  element.addEventListener('keydown', changeRandomColors);
-};
+})();
--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,17 @@
 'use strict';
 // обьект utils
-window.utils = {
-  'getRandomElement': function (array) {
-    return array[Math.floor(Math.random() * array.length)];
-  },
-  'getRandomElementExcept': function (array, element) {
-    var newElement = element;
-    while (newElement === element) {
-      newElement = this.getRandomElement(array);
+window.utils = (function () {
+
+  return {
+    'getRandomElement': function (array) {
+      return array[Math.floor(Math.random() * array.length)];
+    },
+    'getRandomElementExcept': function (array, element) {
+      var newElement = element;
+      while (newElement === element) {
+        newElement = this.getRandomElement(array);
+      }
+      return newElement;
     }
-    return newElement;
-  }
-};
+  };
+})();
