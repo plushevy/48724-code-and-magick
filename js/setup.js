3c2eafd0dc218be22781e5b512616e71e11f4b0e
Илья Молчанов <plue@inbox.ru>
2017-02-22 16:57:40 +0300

    создал новый массив из случайных элементов, нарисовал клон и вывел на страницуt

--- js/setup.js
+++ js/setup.js
@@ -10,8 +10,9 @@
   var wizardEyes = wizard.querySelector('#wizard-eyes');
   var fireball = setup.querySelector('.setup-fireball-wrap');
   var buttonSave = setup.querySelector('.button.setup-submit');
-  var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/code-and-magick/data11';
+  var DATA_URL = 'https://intensive-javascript-server-myophkugvq.now.sh/code-and-magick/data';
   var wizards;
+  var setupSimiliar = document.querySelector('.setup-similar');
 
 
   // валидация полей формы имени пользователя
@@ -44,6 +45,69 @@
     '#e848d5',
     '#e6e848'
   ];
+
+  var drawNewCloneWizard = function () {
+
+    // массив из 5 случайных
+    var makeNewArrayFromRandomEl = function (arr1, maxLength) {
+      var arr2 = [];
+      while (arr2.length < maxLength) {
+        var randElement = window.utils.getRandomElement(arr1);
+        if (arr2.indexOf(randElement) === -1) {
+          arr2.push(randElement);
+        }
+      }
+      return arr2;
+    };
+
+    var newWizards = makeNewArrayFromRandomEl(wizards, 5);
+    var setupWizardSvg = document.querySelector('.setup-wizard');
+
+    // клонируем и рисуем копию мага
+    var render = function (wizardData) {
+      var cloneWizard = setupWizardSvg.cloneNode(true);
+
+      cloneWizard.style.display = 'inline-block';
+      cloneWizard.style.position = 'static';
+      cloneWizard.setAttribute('title', wizardData.name);
+
+      // меняем id на class
+      var changeAttrIdtoClass = function () {
+        var arr = cloneWizard.querySelectorAll('[id]');
+        arr.forEach(function (el) {
+          var value = el.getAttribute('id');
+          el.removeAttribute('id');
+          el.setAttribute('class', value);
+        });
+      };
+      changeAttrIdtoClass();
+
+      cloneWizard.style.position = 'static';
+      cloneWizard.style.width = 50;
+      cloneWizard.style.height = 50;
+      cloneWizard.querySelector('.wizard-coat').style.fill = wizardData.colorCoat;
+      cloneWizard.querySelector('.wizard-eyes').style.fill = wizardData.colorEyes;
+
+      return cloneWizard;
+    };
+
+    // вставляем в фрагмент наши 5 клонов
+    var fragment = document.createDocumentFragment();
+    newWizards.forEach(function (item) {
+      fragment.appendChild(render(item));
+    });
+    setupSimiliar.innerHTML = '';
+    setupSimiliar.appendChild(fragment);
+  };
+
+  // load()
+  var loadAndDrawData = function () {
+    window.load(DATA_URL, function (data) {
+      wizards = JSON.parse(data);
+      drawNewCloneWizard();
+    });
+  };
+
 
   window.enableSetup = (function () {
     var onSetupClose = null;
@@ -85,17 +149,14 @@
       }
     };
 
+
     var closeOnSetupClick = function (event) {
       closeOverlay();
     };
 
     var openOverlay = function (callback) {
       showOverlay();
-      // load()
-      window.load(DATA_URL, function (data) {
-        wizards = JSON.parse(data);
-        console.log(wizards);
-      });
+      loadAndDrawData();
       setupClose.addEventListener('keydown', closeOnSetupKeydown);
       setupClose.addEventListener('click', closeOnSetupClick);
 
@@ -122,6 +183,7 @@
     } else {
       element.style.background = color;
     }
+    setTimeout(drawNewCloneWizard, 5000);
   };
 
   window.colorizeElement(wizardCoat, wizardCoatColors, colorSetter);
